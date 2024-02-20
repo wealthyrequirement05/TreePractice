2d732e4d01937496aa75b950318c9d9d4077433a
wealthyrequirement05 <152629531+wealthyrequirement05@users.noreply.github.com>
2024-02-19 19:08:31 -0500

    Create generate_tree.py
    This contains methods that allow the tree to be generated using the tree module. The next step is to have the user modify them.

---
+++ generate_tree.py
@@ -0,0 +1,21 @@
+#This is for generating and displaying binary search trees
+#The next step will be user modification after the tree is generated
+
+import tree.py
+
+def generate_bst(connections):
+    bst = BST()
+    for val in connections:
+        bst.insert(val)
+    return bst
+
+def display_bst(node, level=0):
+    if node is not None:
+        display_bst(node.right, level + 1)
+        print(' ' * 4 * level + '->', node.val)
+        display_bst(node.left, level + 1)
+
+# Usage:
+connections = [10, 5, 15, 2, 7, 12, 18]
+bst = generate_bst(connections)
+display_bst(bst)
